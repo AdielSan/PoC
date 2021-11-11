eefba7862230f9b96a148bbea69f2092ec5d9cc6
AdielSan <adieltab16@gmail.com>
2021-11-11 13:30:16 -0300

    add comentarios

--- network.py
+++ network.py
@@ -6,20 +6,23 @@ from p4utils.mininetlib.network_API import NetworkAPI
 net = NetworkAPI()
 
 #Definição da rede
+#Switches
 net.addP4Switch('s1')
 net.addP4Switch('s2')
 net.addP4Switch('s3')
 net.addP4Switch('s4')
 net.addP4Switch('s5')
 
+ #Hosts
 net.addHost('h2')
 
+#Conexões entre os dispositivos da rede.
 net.addLink("", "")
 
 
 #Opção gerais dos nós
-net.enableCpuPortAll()
-net.disablePcapDumpAll()
-net.disableLogAll()
-net.enableCli()
-net.startNetwork()
+net.enableCpuPortAll()     #
+net.disablePcapDumpAll()   #
+net.disableLogAll()        #
+net.enableCli()            #
+net.startNetwork()         #
