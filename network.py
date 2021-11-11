101763d279698898c79c65896d656ebb8b46d595
AdielSan <adieltab16@gmail.com>
2021-11-11 01:41:00 -0300

    definição de rede

--- network.py
+++ network.py
@@ -2,5 +2,16 @@
 
 from p4utils.mininetlib.network_API import NetworkAPI
 
-#insta
+#Definições gerais da rede
 net = NetworkAPI()
+
+#Definição da rede
+net.addP4Switch('s1')
+net.addP4Switch('s2')
+net.addP4Switch('s3')
+net.addP4Switch('s4')
+net.addP4Switch('s5')
+
+net.addHost('h2')
+
+net.addLink("", "")
