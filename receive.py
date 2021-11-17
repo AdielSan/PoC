4d29323030a20fa654ea38af1ba0636a2cf78f5b
AdielSan <adieltab16@gmail.com>
2021-11-17 12:40:28 -0300

    inicio dos scripts de envio e recebimento de msgs

---
+++ receive.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+import struct
+import os
+
+from scapy.all import sniff, sendp, hexdump, get_if_list, get_if_hwaddr
+from scapy.all import Packet, IPOption
+from scapy.all import ShortField, IntField, LongField, BitField, FieldListField, FieldLenField
+from scapy.all import IP, UDP, Raw
+from scapy.layers.inet import _IPOption_HDR
---
+++ send.py
@@ -0,0 +1,9 @@
+import argparse
+import sys
+import socket
+import random
+import struct
+
+from scapy.all import sendp, send, get_if_list, get_if_hwaddr
+from scapy.all import Packet
+from scapy.all import Ether, IP, UDP, TCP
